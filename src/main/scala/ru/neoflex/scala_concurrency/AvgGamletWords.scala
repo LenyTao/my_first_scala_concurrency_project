7cd32171409397ac031fc8ae9cf5c24b346e2dfd
LenyTao <60776581+LenyTao@users.noreply.github.com>
2021-01-12 20:25:52 +0400

    Commit after the third review

--- src/main/scala/ru/neoflex/scala_concurrency/AvgGamletWords.scala
+++ src/main/scala/ru/neoflex/scala_concurrency/AvgGamletWords.scala
@@ -4,67 +4,77 @@ import java.nio.file.{Files, Path}
 
 
 object AvgGamletWords extends App {
-  val avg = new AvgGamlet
- new ThreadsReadingPartsOfBook(3)
+  val fileForReader: Path = Path.of("./textGamlet/Gamlet.txt")
 
-  val result = avg.getSumWord() / avg.getWordCount()
-  println("Average length of English words: " + String.format("%.1f",result) + " letters")
+  def readWords(): Array[String] = {
+    Files
+      .readString(fileForReader)
+      .replaceAll("\\p{Punct}", "")
+      .replaceAll("\n", " ")
+      .split(" ")
+      .filter(_.nonEmpty)
+      .filter(_ (0).toInt != 13)
+  }
+
+  val clearGamlet = readWords()
+  val avg = new AvgGamlet
+  ThreadsReadingPartsOfBook(5)
+
+  val result: Double = avg.getSumWord() / avg.getWordCount()
+  println("Average length of English words: " + String.format("%.1f", result) + " letters")
   result
 
-  trait ReaderGamlet {
-    val fileForReader: Path = Path.of("./textGamlet/Gamlet.txt")
 
-    def readWords(): Array[String] = {
-      Files.readString(fileForReader).replaceAll("\\p{Punct}", "").replaceAll("\n", " ").split(" ").filter(_.nonEmpty).filter(_ (0).toInt != 13)
-    }
-  }
+  def ThreadsReadingPartsOfBook(numberOfParts: Int) {
+    val arrayThreads: Array[Thread] = new Array[Thread](numberOfParts)
+    val partBookForThread: Array[String] = Array.fill(clearGamlet.length / numberOfParts)("")
+    for (numberThreadWorkingWithPartText <- 0 until numberOfParts) {
+      Array.copy(clearGamlet, numberThreadWorkingWithPartText * partBookForThread.length, partBookForThread, 0, partBookForThread.length)
 
-  class ThreadsReadingPartsOfBook(part: Int) extends ReaderGamlet {
-    val arrayThreads: Array[Thread] = new Array[Thread](part)
-    val partBookForThread: Array[String] = Array.fill(readWords().length / part)("")
-    for (numberThreadWorkingWithPartText <- 0 until part) {
-      Array.copy(readWords(), numberThreadWorkingWithPartText * partBookForThread.length, partBookForThread, 0, partBookForThread.length)
-      arrayThreads.update(
-        numberThreadWorkingWithPartText, new Thread(s"Thread # $numberThreadWorkingWithPartText") {
-          override def run(): Unit = {
-            for (word <- partBookForThread) {
-              avg.inkrementWord()
-              avg.sumWord(word.length.toDouble)
-            }
-          }
-        }
-      )
-        arrayThreads(numberThreadWorkingWithPartText).start()
-      Thread.sleep(1000)
+       arrayThreads.update(
+         numberThreadWorkingWithPartText, new Thread(s"Thread # $numberThreadWorkingWithPartText") {
+           override def run(): Unit = {
+             for (word <- partBookForThread) {
+               avg.inkrementWord()
+               avg.sumWord(word.length)
+             }
+           }
+         }
+       )
+      arrayThreads(numberThreadWorkingWithPartText).start()
+            Thread.sleep(10)
     }
-    for (numThread <- 0 until part) {
+    for (numThread <- 0 until numberOfParts) {
       arrayThreads(numThread).join()
     }
-    val tailOfBook: Int = readWords().length - (part * partBookForThread.length)
+    val tailOfBook: Int = clearGamlet.length - (numberOfParts * partBookForThread.length)
     if (tailOfBook != 0) {
-      readWords().drop(readWords().length - tailOfBook).foreach(x => avg.inkrementWord())
-      readWords().drop(readWords().length - tailOfBook).foreach(x => avg.sumWord(x.length.toDouble))
+      clearGamlet.drop(clearGamlet.length - tailOfBook).foreach(x => avg.inkrementWord())
+      clearGamlet.drop(clearGamlet.length - tailOfBook).foreach(x => avg.sumWord(x.length))
     }
   }
 
-  class AvgGamlet extends ReaderGamlet {
+  class AvgGamlet {
     @volatile private var wordCount: Int = 0
-    @volatile private var sumWord: Double = 0.0
+    @volatile private var sumWord: Int = 0
 
-    def inkrementWord(): Unit = {
+    def inkrementWord(): Int = {
       wordCount += 1
+      wordCount
     }
 
-    def getWordCount(): Double = {
-      wordCount.toDouble
+    def getWordCount(): Int = {
+      wordCount
     }
 
-    def sumWord(wordLength: Double): Unit = {
+    def sumWord(wordLength: Int): Int = {
       sumWord += wordLength
+      sumWord
     }
 
     def getSumWord(): Double = {
       sumWord
     }
   }
+
 }
